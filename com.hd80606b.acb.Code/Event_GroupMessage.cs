92488c8fe84feabcc470266911060656eb4efe34
“hd80606b <772711681@qq.com>
2020-03-30 22:03:57 +0800

    上传

---
+++ com.hd80606b.acb.Code/Event_GroupMessage.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using com.hd80606b.cq.Code.ExtClass;
+using Native.Sdk.Cqp;
+using Native.Sdk.Cqp.EventArgs;
+using Native.Sdk.Cqp.Interface;
+using Native.Sdk.Cqp.Model;
+using System.Web.Script.Serialization;
+
+namespace com.hd80606b.acb.Code
+{
+    public class Event_GroupMessage : IGroupMessage
+    {
+        /// <summary>
+        /// 收到群消息
+        /// </summary>
+        /// <param name="sender">事件来源</param>
+        /// <param name="e">事件参数</param>
+        public void GroupMessage(object sender, CQGroupMessageEventArgs e)
+        {
+            #region---AV号与BV号的转换---
+            if (e.Message.Text.Contains("bilibili.com/video/BV"))
+            {
+                {
+                    try
+                    {
+                        string bv_replace = "/BV(.){9,10}";
+                        string bv = System.Text.RegularExpressions.Regex.Match(e.Message.Text, bv_replace).ToString();
+                        bv = bv.Replace("/", "");
+                        WebClient wb = new WebClient();
+                        byte[] DownloadData = wb.DownloadData("http://api.bilibili.com/x/web-interface/archive/stat?bvid=" + bv);
+                        string EncodeStr = System.Text.Encoding.UTF8.GetString(DownloadData);
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                        bilibiliClass.Rootobject root = js.Deserialize<bilibiliClass.Rootobject>(EncodeStr);
+                        e.CQApi.SendGroupMessage(e.FromGroup, CQApi.CQCode_At(e.FromQQ), "AV号：av" + root.data.aid + "\nBV号：" + root.data.bvid + "\n播放量：" + root.data.view + "\n收藏量：" + root.data.favorite);
+                    }
+                    catch
+                    {
+                        e.CQApi.SendGroupMessage(e.FromGroup, CQApi.CQCode_At(e.FromQQ) + "哦豁，出错了。");
+                        e.Handler = false;
+                        return;
+                    }
+                }
+            }
+
+            if (e.Message.Text.Contains("bilibili.com/video/av"))
+            {
+                {
+                    try
+                    {
+                        string av_replace = "/av(.){1,10}";
+                        string av = System.Text.RegularExpressions.Regex.Match(e.Message.Text, av_replace).ToString();
+                        av = av.Replace("/av", "");
+                        WebClient wb = new WebClient();
+                        byte[] DownloadData = wb.DownloadData("http://api.bilibili.com/x/web-interface/archive/stat?aid=" + av);
+                        string EncodeStr = System.Text.Encoding.UTF8.GetString(DownloadData);
+                        JavaScriptSerializer js = new JavaScriptSerializer();
+                        bilibiliClass.Rootobject root1 = js.Deserialize<bilibiliClass.Rootobject>(EncodeStr);
+                        e.CQApi.SendGroupMessage(e.FromGroup, CQApi.CQCode_At(e.FromQQ), "AV号：av" + root1.data.aid + "\nBV号：" + root1.data.bvid + "\n播放量：" + root1.data.view + "\n收藏量：" + root1.data.favorite);
+                    }
+                    catch
+                    {
+                        e.CQApi.SendGroupMessage(e.FromGroup, CQApi.CQCode_At(e.FromQQ) + "哦豁，出错了。");
+                        e.Handler = false;
+                        return;
+                    }
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ com.hd80606b.acb.Code/ExtClass/bilibiliClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hd80606b.cq.Code.ExtClass
+{
+    class bilibiliClass
+    {
+
+        public class Rootobject
+        {
+            public int code { get; set; }
+            public string message { get; set; }
+            public int ttl { get; set; }
+            public Data data { get; set; }
+        }
+
+        public class Data
+        {
+            public int aid { get; set; }
+            public string bvid { get; set; }
+            public int view { get; set; }
+            public int danmaku { get; set; }
+            public int reply { get; set; }
+            public int favorite { get; set; }
+            public int coin { get; set; }
+            public int share { get; set; }
+            public int like { get; set; }
+            public int now_rank { get; set; }
+            public int his_rank { get; set; }
+            public int no_reprint { get; set; }
+            public int copyright { get; set; }
+            public string argue_msg { get; set; }
+            public string evaluation { get; set; }
+        }
+
+    }
+}
